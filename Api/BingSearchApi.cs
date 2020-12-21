247d9e3830eaccfd1e97d99bf0fd9701dd7e278f
Federico Sciarabba <yu.gi99@hotmail.it>
2020-12-21 12:09:57 +0100

    Added better google api and Bing api, GG me

--- Api/BingSearchApi.cs
+++ Api/BingSearchApi.cs
@@ -2,15 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Google.Apis.Customsearch.v1;
-using Google.Apis.Services;
 using Newtonsoft.Json;
 using RTFM_SearchEngine.Models;
 
 namespace RTFM_SearchEngine.Api
 {
-
-    public class GoogleSearchApi : ISearchApi
+    public class BingSearchApi : ISearchApi
     {
         public async Task<List<SearchResult>> GetSearchResultAsync(string searchText)
         {
@@ -22,11 +19,12 @@ namespace RTFM_SearchEngine.Api
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri($"https://google-search5.p.rapidapi.com/google-serps/?q={cleanedSearchText}&gl=it&hl=it-IT"),
+                RequestUri = new Uri($"https://bing-web-search1.p.rapidapi.com/search?q={cleanedSearchText}&mkt=it-it"),
                 Headers =
                 {
+                    { "x-bingapis-sdk", "true" },
                     { "x-rapidapi-key", "1c2eb8ab42msh25eb17f236d532dp15b8dejsne52d0aab7277" },
-                    { "x-rapidapi-host", "google-search5.p.rapidapi.com" },
+                    { "x-rapidapi-host", "bing-web-search1.p.rapidapi.com" },
                 },
             };
             using (var response = await client.SendAsync(request))
@@ -35,11 +33,11 @@ namespace RTFM_SearchEngine.Api
                 var body = await response.Content.ReadAsStringAsync();
                 dynamic jsonData = JsonConvert.DeserializeObject(body);
                 var results = new List<SearchResult>();
-                foreach (var item in jsonData.data.results.organic)
+                foreach (var item in jsonData.webPages.value)
                 {
                     results.Add(new SearchResult
                     {
-                        Title = item.title,
+                        Title = item.name,
                         Link = item.url,
                         Description = item.snippet
                     });
--- Api/GenericSearchApi.cs
+++ Api/GenericSearchApi.cs
@@ -2,15 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Google.Apis.Customsearch.v1;
-using Google.Apis.Services;
 using Newtonsoft.Json;
 using RTFM_SearchEngine.Models;
 
 namespace RTFM_SearchEngine.Api
 {
-
-    public class GoogleSearchApi : ISearchApi
+    public class GenericSearchApi : ISearchApi
     {
         public async Task<List<SearchResult>> GetSearchResultAsync(string searchText)
         {
@@ -22,12 +19,12 @@ namespace RTFM_SearchEngine.Api
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri($"https://google-search5.p.rapidapi.com/google-serps/?q={cleanedSearchText}&gl=it&hl=it-IT"),
+                RequestUri = new Uri($"https://webknox-search.p.rapidapi.com/webpage/search?query={cleanedSearchText}&number=10&language=en"),
                 Headers =
-                {
-                    { "x-rapidapi-key", "1c2eb8ab42msh25eb17f236d532dp15b8dejsne52d0aab7277" },
-                    { "x-rapidapi-host", "google-search5.p.rapidapi.com" },
-                },
+    {
+        { "x-rapidapi-key", "1c2eb8ab42msh25eb17f236d532dp15b8dejsne52d0aab7277" },
+        { "x-rapidapi-host", "webknox-search.p.rapidapi.com" },
+    },
             };
             using (var response = await client.SendAsync(request))
             {
@@ -35,13 +32,11 @@ namespace RTFM_SearchEngine.Api
                 var body = await response.Content.ReadAsStringAsync();
                 dynamic jsonData = JsonConvert.DeserializeObject(body);
                 var results = new List<SearchResult>();
-                foreach (var item in jsonData.data.results.organic)
+                foreach (var item in jsonData.results)
                 {
-                    results.Add(new SearchResult
-                    {
+                    results.Add(new SearchResult{
                         Title = item.title,
-                        Link = item.url,
-                        Description = item.snippet
+                        Link = item.url
                     });
                 }
                 return results;
