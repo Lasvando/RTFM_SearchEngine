da98a84759077bb166d7c1d330ba9f27743d3bb4
Federico Sciarabba <yu.gi99@hotmail.it>
2020-12-21 12:49:34 +0100

    null

--- Api/ApiKeyHidden.cs
+++ Api/ApiKeyHidden.cs
@@ -2,7 +2,7 @@ namespace RTFM_SearchEngine.Api
 {
     public class ApiKeyHidden
     {
-        const string BING_API_KEY = "1c2eb8ab42msh25eb17f236d532dp15b8dejsne52d0aab7277";
-        const string GOOGLE_API_KEY = "1c2eb8ab42msh25eb17f236d532dp15b8dejsne52d0aab7277";
+        public const string BING_API_KEY = "1c2eb8ab42msh25eb17f236d532dp15b8dejsne52d0aab7277";
+        public const string GOOGLE_API_KEY = "1c2eb8ab42msh25eb17f236d532dp15b8dejsne52d0aab7277";
     }
 }
